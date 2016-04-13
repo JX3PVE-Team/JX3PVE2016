4b0171824859825a8161a73f2a0dbe56080c1d28
17229398@qq.com <17229398@qq.com>
2016-04-13 20:03:28 +0800

    公共嵌入底

--- js/__output/mod/footer.js
+++ js/__output/mod/footer.js
@@ -1,1 +1,1 @@
-H.ready(["jquery"],function(){jQuery(function(o){var c=(o("#c-float-wx-tg"),o("#c-float-wx-code"),o("#c-float-gotop")),l=o("#c-float-close"),n=o("#c-float-help");c.on("click",function(){o(window).scrollTop(0)}),l.on("click",function(){n.hide()})})});
+H.ready(["jquery"],function(){jQuery(function(o){var c=o("#c-float-gotop"),n=o("#c-float-close"),l=o("#c-float-help");c.on("click",function(){o(window).scrollTop(0)}),n.on("click",function(){l.hide()})})});
--- js/mod/footer.js
+++ js/mod/footer.js
@@ -1,18 +1,9 @@
 H.ready(['jquery'], function(){
     jQuery(function($){
 
-    	var $wx_tg = $("#c-float-wx-tg"),
-    		$wx_img = $("#c-float-wx-code"),
-    		$gotop = $("#c-float-gotop"),
+    	var $gotop = $("#c-float-gotop"),
             $close = $("#c-float-close"),
             $fwin = $("#c-float-help")
-
-    	//浮层·微信
-    	/*$wx_tg.hover(function(){
-    		$wx_img.fadeIn()
-    	},function(){
-    		$wx_img.fadeOut()
-    	})*/
 
     	//浮层·回到顶部
     	$gotop.on('click',function(){
