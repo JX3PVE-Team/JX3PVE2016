f31d00694d2594105f12ad8d59c8f060cabd2642
17229398@qq.com <17229398@qq.com>
2016-04-26 01:45:40 +0800

    面包屑简化、编辑器浮层定位修复

--- js/page/pve-mix.js
+++
@@ -1,6 +0,0 @@
-jQuery(function($) {
-
-	//侧边栏
-	//fixSidebar('.pve-sidebar', 110, 105, 100)
-
-})
--- source/module/forum/forum_forumdisplay.php
+++ source/module/forum/forum_forumdisplay.php
@@ -73,7 +73,7 @@ $forum_up = $_G['cache']['forums'][$_G['forum']['fup']];
 if($_G['forum']['type'] == 'forum') {
 	$fgroupid = $_G['forum']['fup'];
 	if(empty($_GET['archiveid'])) {
-		$navigation = ' <em>&rsaquo;</em> <a href="forum.php?gid='.$forum_up['fid'].'">'.$forum_up['name'].'</a><em>&rsaquo;</em> <a href="forum.php?mod=forumdisplay&fid='.$_G['forum']['fid'].'">'.$_G['forum']['name'].'</a>';
+		$navigation = ' <em>&rsaquo;</em> <a href="forum.php?mod=forumdisplay&fid='.$_G['forum']['fid'].'">'.$_G['forum']['name'].'</a>';
 	} else {
 		$navigation = ' <em>&rsaquo;</em> '.'<a href="forum.php?mod=forumdisplay&fid='.$_G['fid'].'">'.$_G['forum']['name'].'</a> <em>&rsaquo;</em> '.$forumarchive[$_GET['archiveid']]['displayname'];
 	}
@@ -82,7 +82,7 @@ if($_G['forum']['type'] == 'forum') {
 	$fgroupid = $forum_up['fup'];
 	if(empty($_GET['archiveid'])) {
 		$forum_top =  $_G['cache']['forums'][$forum_up[fup]];
-		$navigation = ' <em>&rsaquo;</em> <a href="forum.php?gid='.$forum_top['fid'].'">'.$forum_top['name'].'</a><em>&rsaquo;</em> <a href="forum.php?mod=forumdisplay&fid='.$forum_up['fid'].'">'.$forum_up['name'].'</a><em>&rsaquo;</em> '.$_G['forum']['name'];
+		$navigation = ' <em>&rsaquo;</em> <a href="forum.php?mod=forumdisplay&fid='.$forum_up['fid'].'">'.$forum_up['name'].'</a><em>&rsaquo;</em> '.$_G['forum']['name'];
 	} else {
 		$navigation = ' <em>&rsaquo;</em> <a href="forum.php?mod=forumdisplay&fid='.$_G['forum']['fup'].'">'.$forum_up['name'].'</a> <em>&rsaquo;</em> '.'<a href="forum.php?mod=forumdisplay&fid='.$_G['fid'].'">'.$_G['forum']['name'].'</a> <em>&rsaquo;</em> '.$forumarchive[$_GET['archiveid']]['displayname'];
 	}
