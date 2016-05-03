479018c3beab18880591a33b8047f0a70c27fb38
17229398@qq.com <17229398@qq.com>
2016-05-04 04:02:21 +0800

    个人中心3/4

--- js/page/page-bbs.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($) {
-
-	//树形菜单
-	//$("#jx3pve-map").treeview();
-
-	//侧边栏
-	//fixSidebar('.pve-sidebar', 110, 105, 100)
-
-})
---
+++ js/user/index.js
@@ -0,0 +1,11 @@
+jQuery(function($){
+	var user_email_status = $.trim($('#user-email-status').text()).indexOf('已验证') != -1;
+	if(user_email_status){
+		$('.user-os-profile .u-mail').addClass('validate');
+		$('#user-email-status a').css('color','green');
+	}else{
+		$('.user-os-profile .u-mail').addClass('novalidate');
+		$('#user-email-status a').css('color','#FF7D00');
+		$('#user-email-status').wrap('<a href="http://www.jx3pve.com/home.php?mod=spacecp&ac=profile&op=password"></a>');
+	}
+});  
