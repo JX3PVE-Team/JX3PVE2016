30334ff0230097fcf75388fdacc4d5938679b42e
unknown <17229398@qq.com>
2016-05-12 12:13:11 +0800

    Signed-off-by: unknown <17229398@qq.com>

--- js/widget/w-showbox.js
+++ js/widget/w-showbox.js
@@ -1,45 +1,50 @@
 jQuery(function($){
     
-    //查看脸型原图 imgorigin
     var
-    $html = $("html"),
     $imgorigin = $(".u-imgorigin"),
-    $mask = $("#u-mask"),
     $showbox = $("#c-showbox"),
-    $boxclose = $("#c-showbox-close"),
-    $imgbox = $("#c-showbox-content")
-    $imgorigin.on('click',function(){
-        var img = $(this).find('img'),
-            src = img.attr('src')
-        if(src && src.indexOf('nophoto')==-1){
-            $mask.fadeIn()
-            $html.addClass('fixpage')
-            $imgbox.append('<img id="c-showbox-img" src="'+ src + '"/>')
-            var box_w = $showbox.outerWidth(),
-                box_h = $showbox.outerHeight(),
-                ct_w = $showbox.width(),
-                ct_h = $showbox.height(),
-                offset_x = -box_w/2,
-                offset_y = -box_h/2
-            $imgbox.height(ct_h).width(ct_w)
-            $showbox.fadeIn().addClass('c-showbox-show').css({
-                'margin-left': offset_x,
-                'margin-top': offset_y
+    $close = $("#c-showbox-close")
+
+    var padding = 28
+
+    function createImgDOM(){
+        $showbox.append('<img class="c-showbox-img" id="c-showbox-img" src="" />')
+    }
+
+    $imgorigin.each(function(){
+        var img = $(this).find('img')
+        var src = $(this).find('img').attr('src')
+        var isEmpty = !src || src.indexOf('nophoto')!=-1
+        if(isEmpty){
+            $(this).addClass('isEmpty')
+        }else{
+            $(this).on('click',function(){
+                showMask()
+                createImgDOM()
+                $("#c-showbox-img").attr('src',src)
+                
+                var 
+                    offset_x = - ($showbox.width() + padding*2 )/2,
+                    offset_y = - ($showbox.height() + padding*2 )/2
+
+                $showbox.fadeIn().addClass('c-showbox-show').css({
+                    'margin-left': offset_x,
+                    'margin-top': offset_y
+                })
+
             })
         }
     })
-    //脸型原图
-    $boxclose.on('click',function(){
-        $mask.fadeOut()
-        $html.removeClass('fixpage')
+
+    $close.on('click',function(){
+        hideMask()
         $showbox.fadeOut()
         $('#c-showbox-img').remove()
     })
 
     if(isMobile){
         $showbox.on('click',function(){
-            $mask.fadeOut()
-            $html.removeClass('fixpage')
+            hideMask()
             $(this).fadeOut()
             $('#c-showbox-img').remove()
         })
