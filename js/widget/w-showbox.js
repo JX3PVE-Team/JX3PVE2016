447554fab27516d3a88d66ee3a48954b40a30b66
unknown <17229398@qq.com>
2016-05-13 11:06:47 +0800

    showbox问题

--- js/widget/w-showbox.js
+++ js/widget/w-showbox.js
@@ -3,9 +3,8 @@ jQuery(function($){
     var
     $imgorigin = $(".u-imgorigin"),
     $showbox = $("#c-showbox"),
+    $imgbox = $("#c-showbox-content"),
     $close = $("#c-showbox-close")
-
-    var padding = 28
 
     $imgorigin.each(function(){
         var src = $(this).find('img').attr('src')
@@ -15,34 +14,36 @@ jQuery(function($){
         }else{
             $(this).on('click',function(){
                 showMask()
-                $(this).find('img').clone().attr('id','c-showbox-img').attr('class','c-showbox-img').appendTo($showbox)
+                var src= $(this).find('img').attr('src')
+                $imgbox.append('<img class="c-showbox-img" id="c-showbox-img"/>')
+                var $img = $("#c-showbox-img")
+                $img.attr('src',src)
                 $showbox.fadeIn()
 
+                //在display:block后正常获得图片大小后再进行位移
                 var 
                 offset_x = - $showbox.outerWidth()/2,
                 offset_y = - $showbox.outerHeight()/2
-
                 $showbox.css({
                     'margin-left': offset_x,
                     'margin-top': offset_y
                 })
+                $showbox.addClass('isShow')
+
+                //图片大小不受控问题
+                var w = $showbox.width()
+                var h = $showbox.height()
+                $img.width(w)
+                $img.height(h)
 
             })
         }
     })
 
-    $close.on('click',function(){
+    $close.add($showbox).on('click',function(){
         hideMask()
-        $showbox.fadeOut()
         $('#c-showbox-img').remove()
+        $showbox.fadeOut()
     })
-
-    if(isMobile){
-        $showbox.on('click',function(){
-            hideMask()
-            $(this).fadeOut()
-            $('#c-showbox-img').remove()
-        })
-    }
 
 })
