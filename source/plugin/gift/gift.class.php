c1faed0789b962e5f67e5fa178d1d323e71cc52e
unknown <17229398@qq.com>
2016-06-01 11:13:00 +0800

    积分兑换后台页面

---
+++ source/plugin/gift/gift.class.php
@@ -0,0 +1,15 @@
+<?php
+	if(!defined('IN_DISCUZ')) {
+	exit('Access Deined');
+}
+
+
+class plugin_gift {
+	var $uid;
+	function __construct(){
+		global $_G;
+		$this->uid=$_G['uid'];
+	}
+	
+	}
+?>
