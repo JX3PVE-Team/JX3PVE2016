83c2a205e491eff7abe5487ee195934945a1dbba
unknown <17229398@qq.com>
2016-10-12 16:56:01 +0800

    os login

---
+++ api/member/login.php
@@ -0,0 +1,35 @@
+<?php
+
+
+require_once '../../source/class/class_core.php';
+$discuz = C::app();
+$discuz->init();
+
+$username = addslashes($_GET["username"]);
+$password = addslashes($_GET['password']);
+
+if (!$username || !$password) {
+    echo "[用户名密码不能为空, 0]";
+    die();
+}
+
+$user = C::t('common_member')->fetch_by_username($username);
+if (!$user) {
+    echo "[用户不存在, 0]";
+    die();
+}
+
+$user = C::t('common_member')->get_uc_user_by_username($username);
+if (!$user) {
+    echo "[用户不存在, 0]";
+    die();
+}
+
+if($user['password'] != md5(md5($password).$user['salt'])) {
+    echo "[密码错误, 0]";
+    die();
+}
+
+
+echo "[登陆成功,".$user['uid']."]";
+die();
